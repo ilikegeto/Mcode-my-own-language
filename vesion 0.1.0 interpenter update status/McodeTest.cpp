40e48d38360994d135c88c3c1685d694e88016d1
ilikegeto <rafalexa63@gmail.com>
2026-06-27 12:03:25 +0700

    Add files via upload

---
+++ vesion 0.1.0 interpenter update status/McodeTest.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "mayowencode.cpp"
+
+int main() {
+    PipaCompiler PipaCompiler;
+    //PipaCompiler.PipaToExe("PipaGuide.pipa", "PPLGD");
+    PipaCompiler.PipaToExe("Ppipaproject.pipa", "calculatorpipa");
+    return 0; 
+}
